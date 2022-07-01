bd31f7a387f643d6b1b2912facaea25f1cf91312
Juan Moldes <juanignaciomoldes1@gmail.com>
2022-07-01 17:54:01 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header_title">Hi!</h1>
+      <h1 className="header_title">My name is Juam Moldes</h1>
+      <h1 className="header_title">I'm a web developer</h1>
+    </header>
+  );
+}
+
