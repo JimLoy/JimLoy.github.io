e878d63b495c077a2c0bfa458f0e1689eefff20d
Juan Moldes <juanignaciomoldes1@gmail.com>
2022-07-14 17:17:56 -0300

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div className="App">
       <Header/>
+      <About/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
