80e53e96404740d4dccb05474cd47c59d145cbd4
Juan Moldes <juanignaciomoldes1@gmail.com>
2022-07-19 17:52:03 -0300

    add project data

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,17 @@
-export default function ProjectCard({name,img,description,repository,deploy}){
+import weather from '../assets/bkg2.jpg';
+import gimme from '../assets/gimmeARide.png';
+import country from '../assets/landingPageWallpaper.jpg';
+
+
+export default function ProjectCard({name,description,repository,deploy}){
   return(
     <div>
       <h2>{name}</h2>
-      <img src={`../assets/${img}`} alt="Project" />
+      <img src={name === "PI Countries" ? 
+        country 
+        : name === "PG Gimme a Ride" ? 
+        gimme 
+        : weather} alt={`Project ${name}`} />
       <p>{description}</p>
       <button><a href={repository} alt="Repository">Repository</a></button>
       <button><a href={deploy} alt="Deploy">Deploy</a></button>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,12 +5,23 @@ export default function Projects(){
     <div>
       <h1 className="title">PROJECTS</h1>
       <ProjectCard 
-        name="PI-Countries" 
-        img="ElandingPageWallpaper.jpg" 
+        name="PI Countries" 
         description="Desarrollo de una Single Page Application usando React.JS y Redux.JS para el Front-End, estilizados con CSS puro sin usar librerías externas. Los datos de una API y el Back-End fueron consumidos usando Node.JS y Express.JS, añadiendo nuevas funcionalidades a la API Pokeapi. Algunas funcionalidades del proyecto son permitir mostrar información característica de diferentes países del mundo de forma ordenada, ya sea por nombre de país o por población, permitiendo filtrar por continente o por el nombre de alguna actividad que se realice así, realizar búsquedas por nombre, y crear nuevas actividades." 
         repository="https://github.com/JimLoy/PI-Countries" 
         deploy="https://pi-countries-jimloy.vercel.app"
-      /> 
+      />
+      <ProjectCard 
+        name="PG Gimme a Ride" 
+        description="Desarrollo grupal de una aplicación web que conecta usuarios que van a un mismo destino, para poder viajar juntos y compartir gastos. Para este proyecto se utilizó: React.js, Redux.js, Node.js, Express.js, PostgreSQL, MercadoPago, Sass, Material UI, Cloudinary, NodeMailer, MapBox GL JS, Auth0, Sequelize" 
+        repository="https://github.com/Marisaliap/PG-Subi" 
+        deploy="https://gimmearide.vercel.app"
+      />
+      <ProjectCard 
+        name="Weather App" 
+        description="El front-end esta desarrollado con React y un reducido back-end con express (en Node). Devuelve la temperatura, velocidad del viento, humedad y sensacion termica de la ciudad que escribas consumiendo la informacion de la api OpenWeather" 
+        repository="https://github.com/JimLoy/Weather-App" 
+        deploy="https://weather-app-jimloy.herokuapp.com"
+      />   
     </div>
   )
 }
