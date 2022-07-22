34efaaad7a87bf00a0f37091a31891790288f3f3
Juan Moldes <juanignaciomoldes1@gmail.com>
2022-07-22 17:50:17 -0300

    rename the sass file variables, and somethings more

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,10 @@ export default function About() {
   return (
     <div className="about">
       <h1 className="about_title">ABOUT ME</h1>
-      <img src={me} alt="Me" className="about_img"/>
-      <p className="about_p" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio libero eligendi magnam doloremque officia dolorem quasi ipsa enim, 
-      voluptatibus reiciendis ab tempore animi molestias, voluptas aliquid natus, at quisquam eos?</p>
+      <div className="about_container">
+        <img src={me} alt="Me" className="about_img"/>
+        <p className="about_p" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio libero eligendi magnam doloremque officia dolorem quasi ipsa enim voluptatibus reiciendis ab tempore animi molestias, voluptas aliquid natus, at quisquam eos?</p>
+      </div>
     </div>
   );
-}
+}                                                                                                                                                                                                                                              
