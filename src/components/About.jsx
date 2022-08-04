0c74da129de4d716eb3483f16db6637ab7b2a013
Juan Moldes <juanignaciomoldes1@gmail.com>
2022-08-04 16:26:27 -0300

    About finished, only the new photo is missing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,12 @@ export default function About() {
       <h1 className="about_title">ABOUT ME</h1>
       <div className="about_container">
         <img src={me} alt="Me" className="about_img"/>
-        <p className="about_p" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio libero eligendi magnam doloremque officia dolorem quasi ipsa enim voluptatibus reiciendis ab tempore animi molestias, voluptas aliquid natus, at quisquam eos?</p>
+        <div className="about_p_container">
+          <p className="about_p" >Nice to meet you!</p>
+          <p className="about_p" >I dedicate my time to expanding my knowledge, learning and developing my skills and enhancing interpersonal relationships.</p>
+          <p className="about_p" >I am a creative, organized and disciplined person, efficient when managing tasks and managing time.</p>
+          <p className="about_p" >I learn in an agile and autonomous way. I easily adapt to changes that arise.</p>
+        </div>
       </div>
     </div>
   );
