ef71d36bda75f9887bf97f33a34c03ed5faffc1c
Juan Moldes <juanignaciomoldes1@gmail.com>
2022-08-16 17:54:29 -0300

    fixed contact form and some classNames

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,22 @@
 export default function Contact(){
+
+  function handleOnSubmit(e){
+    e.preventDefault();
+    const form = new FormData(e.target)
+    const mailto = document.querySelector('#mailto')
+    mailto.setAttribute('href',`mailto:juanignaciomoldes1@gmail.com?subject=${form.get('subject')}&body=${form.get('message')}`)
+    mailto.click()
+  }
+
   return(
-    <form>
-      <input type="text" placeholder="Name"/>
-      <input type="email" placeholder="Email"/>
-      <textarea name="message" cols="30" rows="10"></textarea>
-      <button>Submit</button>
-    </form>
+    <div className="contact">
+      <h2 className="contact_title">Contact</h2>
+      <form className="contact_form" onSubmit={handleOnSubmit}>
+        <input className="contact_input" name='subject' type="text" placeholder="Subject"/>
+        <textarea className="contact_textarea" name="message" cols="30" rows="10" placeholder="Message body"></textarea>
+        <button className="contact_button">Submit</button>
+      </form>
+      <a href='mailto:juanignaciomoldes1@gmail.com' id='mailto'>_</a>
+    </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import {BsGithub,BsLinkedin} from 'react-icons/bs';
 
 export default function Footer(){
   return(
-    <footer>
-      <a href="https://github.com/JimLoy" alt="GitHub"><BsGithub/></a>
-      <a href="https://www.linkedin.com/in/juanignaciomoldes-js/" alt="Linkedin"><BsLinkedin/></a>
+    <footer className='footer'>
+      <a className='footer_icon' href="https://github.com/JimLoy" alt="GitHub"><BsGithub/></a>
+      <a className='footer_icon' href="https://www.linkedin.com/in/juanignaciomoldes-js/" alt="Linkedin"><BsLinkedin/></a>
     </footer>
   )
 }
